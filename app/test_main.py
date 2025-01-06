098b4b8aa3d90f95856e940655c958c83e89db2d
unknown <maximuschampion2002@gmail.com>
2025-01-07 01:09:29 +0200

    check-password

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        ("Abc@1234", True),
+        ("A1b@Cdef", True),
+        ("Abc12345", False),
+        ("Abc@Defgh", False),
+        ("abc@1234", False),
+        ("Abc!@#$%", False),
+        ("A1b@CdEfGhIjKlmNOP", False),
+        ("A1b@CdEfGhIjKlmNOP1I234J", False),
+        ("A1c@t", False)
+    ]
+)
+def test_check_password(password, expected):
+    assert check_password(password) == expected
